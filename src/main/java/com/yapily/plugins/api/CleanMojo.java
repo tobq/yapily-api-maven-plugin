fed7e0979b11ef57de5a3a665a04c3c4e8ea2727
tobi-yapily <tobi.akinyemi@yapily.com>
2021-02-17 17:50:19 +0000

    added clean goal

---
+++ src/main/java/com/yapily/plugins/api/CleanMojo.java
@@ -0,0 +1,24 @@
+package com.yapily.plugins.api;
+
+import java.io.IOException;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.project.MavenProject;
+
+@Mojo(name = "clean")
+public class CleanMojo extends AbstractMojo {
+    @Component
+    private MavenProject project;
+
+    @Override public void execute() throws MojoExecutionException {
+        try {
+            Utils.clean(project);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to clean", e);
+        }
+    }
+}
+
